1e984a0fec5924bf6dad02a5da66a61ae28368a1
zhaoshuyang <694514182@qq.com>
2016-09-12 17:08:21 +0800

    50=>40

--- src/com/scala/test2.scala
+++ src/com/scala/test2.scala
@@ -12,7 +12,7 @@ object test2 {
   	val user_data=sc.textFile("D:/spark-1.5.2-bin-hadoop2.6/data/ml-100k/u.data")
   	val rawRatings=user_data.map {a=>a.split("\t").take(3)}
   	val ratings=rawRatings.map { case Array(user,movie,rating) => Rating(user.toInt,movie.toInt,rating.toDouble) }
-  	val model = ALS.train(ratings,50,10,0.01)
+  	val model = ALS.train(ratings,40,10,0.01)
   	//model.userFeatures.saveAsTextFile("D:/tt")
   	println(model.userFeatures.count())
 	}
